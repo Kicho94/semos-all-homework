6ec894426b4c66b5cfcfa433372eca084d66e987
Stefan Gorgevik <stefangorgevik@hotmail.com>
2019-06-19 17:04:36 +0200

    Update homework1.js

--- node-homework/homework1.js
+++ node-homework/homework1.js
@@ -25,9 +25,9 @@
 // //студентот со најнизок просек,
 //     var b;
 //     var lowestScore = scores.reduce((prev, curr) => curr < prev ? b = curr : prev);
-//     var filtered = people.filter(person => person.prosek <= b);  
-//     console.log("The lowest score belongs to " + filtered[0].ime + " " + filtered[0].prezime + " with a score of: " +lowestScore);
-    
+//     var filtered2 = people.filter(person => person.prosek <= b);  
+//     console.log("The lowest score belongs to " + filtered2[0].ime + " " + filtered2[0].prezime + " with a score of: " +lowestScore);
+ 
 // //и средна вредност (просек) на целата генерација (сите студенти)
 //     var average = scores.reduce((prev, curr) => {                               
 //     return prev + curr;
@@ -35,6 +35,8 @@
 //     var result = average / scores.length;
 //     console.log("The average score of all students is: " + result);
 
+
+//VERSION 2 (with sort)
 
     var students = [
         {ime: "Pero", prezime:"Perovski", prosek: 6.2},
@@ -46,13 +48,25 @@
         {ime: "Cveta", prezime:"Cvetkoska", prosek: 9.2}
     ];
 
-students.sort((a, b) =>  (a.prosek > b.prosek) ? 1 : -1);
-console.log(students);
+    function sort(arr) {
+        students.sort((a, b) =>  (a.prosek > b.prosek) ? 1 : -1);
+    }
+sort(students);
 
-
-var min = students[0].ime + " " + students[0].prezime + " " +  students[0].prosek;
+var min = "Student with the lowest score is: " + students[0].ime + " " + students[0].prezime + " " +  students[0].prosek;
 console.log(min);
 
-// var last = students.slice(-1)[0]
+var last = students.slice(-1)[0]
+var max = "Student with the highest score is: " + last.ime + " " + last.prezime + " " + last.prosek;
+console.log(max);
 
-// console.log(last);
+function average(array) {
+    var scores = array.map(person => person.prosek);
+    var average = scores.reduce((a, b) => {
+        return a + b;
+    })
+    var total = average / students.length;
+    console.log("The average score is: " + total);
+}
+
+average(students);
