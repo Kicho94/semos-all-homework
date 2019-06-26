4d1b836747a66e5e2cbf3971e5651ef61e0fd0bd
Stefan Gorgevik <stefangorgevik@hotmail.com>
2019-06-26 13:39:20 +0200

    Update index.js

--- node-homework/homework3/index.js
+++ node-homework/homework3/index.js
@@ -2,22 +2,22 @@ var http = require('http');
 
 var server = http.createServer(function (req, res) {
    var URL = req.url.split("/");
-   var output = 0; var first = parseInt(URL[2]);
+   var output;
    switch(URL[1]) {
        case "add":
+        output = 0;
            if(URL.length > 2) {
                 for(let i = 2; i < URL.length;i++) {
                     output += parseInt(URL[i]);
                 }
-                res.write("Result for operation " + URL[1] + " is " + output + ".");
-           }
+            }
             break;
         case "sub":
+            output = parseInt(URL[2]);
             if(URL.length >= 2) {     
                 for(let i = 3; i < URL.length;i++) {
-                    first -= parseInt(URL[i]);  
-            }
-            res.write("Result for operation " + URL[1] + " is " + first + ".");
+                    output -= parseInt(URL[i]);  
+                }
             }
             break;
         case "mul":
@@ -26,19 +26,19 @@ var server = http.createServer(function (req, res) {
                 for(let i = 2; i < URL.length;i++) {
                     output *= parseInt(URL[i]);
             }
-            res.write("Result for operation " + URL[1] + " is " + output + ".");
         }
             break;
         case "div":
+            output = parseInt(URL[2]);
             if(URL.length >= 2) {
                 for(let i = 3; i < URL.length; i++) {
-                    first /= parseInt(URL[i]);   
+                    output /= parseInt(URL[i]);   
                 }
-            res.write("Result for operation " + URL[1] + " is " + first + ".");
             }
             break;
    };
-            res.end(); //end the response
+            res.write("Result for operation " + URL[1] + " is " + output);
+            res.end(); 
 });
 
 server.listen(8080, (err) => {
