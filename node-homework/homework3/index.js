927e367d7d10e059256dae8c28cff2cd562d898a
Stefan Gorgevik <stefangorgevik@hotmail.com>
2019-06-26 12:12:28 +0200

    Update index.js

--- node-homework/homework3/index.js
+++ node-homework/homework3/index.js
@@ -1,29 +1,23 @@
-//  https://www.w3schools.com/nodejs/nodejs_http.asp
-
-//Creating a server
-
 var http = require('http');
 
-//create a server object:
 var server = http.createServer(function (req, res) {
-    console.log(req);
-   // res.write('Hello World!'); //write a response to the client
-   // http://127.0.0.1:8080/add/2/3 = 5
-
    var URL = req.url.split("/");
-   var output = 0;
+   var output = 0; var first = parseInt(URL[2]);
    switch(URL[1]) {
        case "add":
            if(URL.length > 2) {
                 for(let i = 2; i < URL.length;i++) {
                     output += parseInt(URL[i]);
                 }
+                res.write("Result for operation " + URL[1] + " is " + output + ".");
            }
             break;
         case "sub":
-            output = 0;
             if(URL.length >= 2) {     
-                        output = parseInt(URL[2]) - parseInt(URL[3]);     
+                for(let i = 3; i < URL.length;i++) {
+                    first -= parseInt(URL[i]);  
+            }
+            res.write("Result for operation " + URL[1] + " is " + first + ".");
             }
             break;
         case "mul":
@@ -32,17 +26,19 @@ var server = http.createServer(function (req, res) {
                 for(let i = 2; i < URL.length;i++) {
                     output *= parseInt(URL[i]);
             }
+            res.write("Result for operation " + URL[1] + " is " + output + ".");
         }
             break;
         case "div":
-            output = 1;
             if(URL.length >= 2) {
-                output = parseInt(URL[2]) / parseInt(URL[3]);
+                for(let i = 3; i < URL.length; i++) {
+                    first /= parseInt(URL[i]);   
+                }
+            res.write("Result for operation " + URL[1] + " is " + first + ".");
             }
             break;
    };
-    res.write(URL[1] + " " + output + "");
-    res.end(); //end the response
+            res.end(); //end the response
 });
 
 server.listen(8080, (err) => {
@@ -53,4 +49,4 @@ server.listen(8080, (err) => {
     console.log("Server started successfully!");
 });
 
-//http://127.0.0.1:8080/ loopback address, local host 
+
